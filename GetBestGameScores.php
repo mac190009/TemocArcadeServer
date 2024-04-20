cc8a888db4fa73e6fab4cb81f389bf72d244f522
mac190009 <99266850+mac190009@users.noreply.github.com>
2024-04-20 03:02:32 -0500

    Create GetBestGameScores.php
    This page takes a game name as an argument and returns a JSON encoded string containing the 10 highest scores and the usernames of the player that made the score.

---
+++ GetBestGameScores.php
@@ -0,0 +1,41 @@
+//This page takes a game name as an argument and returns a JSON encoded string containing the 10 highest scores and the usernames of the player that made the score.
+<?php
+$servername = "localhost";
+$username = "tomec";
+$password = "Utdftw";
+$dbname = "arcade1";
+
+// variables submitted by users
+//$loginUser = $_POST["loginUser"];
+//$loginPass = $_POST["loginPass"];
+$gameName = $_POST["gameName"];
+
+// Create connection
+//$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT p.username, s.score 
+        FROM player AS p INNER JOIN scoreboard AS s
+        ON p.id = s.player_id
+        ORDER BY s.score DESC
+        WHERE s.game= '" . $gameName . "'
+        LIMIT 10";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  $rows = array();
+  while($row = $result->fetch_assoc()) {  // associative array 
+	  $rows[] = $row;
+  }
+  echo json_encode($rows);
+} else {
+  echo "0";
+}
+
+$conn->close();
+?>
