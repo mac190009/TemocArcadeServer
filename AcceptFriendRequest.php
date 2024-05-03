4440a074bc5d2174fef8434be14191bb1ab7a885
mac190009 <99266850+mac190009@users.noreply.github.com>
2024-05-03 02:17:28 -0500

    added close quote

--- AcceptFriendRequest.php
+++ AcceptFriendRequest.php
@@ -9,16 +9,15 @@ $dbname = "arcade1";
 // variables submitted by users
 $frienderID = $_POST["frienderID"];  // ID of player who requested friendship.
 $friendeeID = $_POST["friendeeID"];  // ID of player who received the request (Intended user of this page)
-$requestDate = $_POST["requestDate];  // Date request was made.
+$requestDate = $_POST["requestDate"];  // Date request was made.
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
+if ($conn's connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// not tested
 $sql = "UPDATE friend_request 
         SET status = 'ACCEPTED'
         WHERE playerID1='" . $frienderID . "' AND playerID2='" . $friendeeID . "' AND date='" . $requestDate . "'";
@@ -26,14 +25,14 @@ $sql = "UPDATE friend_request
 if ($conn->query($sql) === TRUE) { 
           echo "Friend request status updated!"; 
     }else { 
-          echo "Error: " . $sql . "<br>" . $conn->error; 
+          echo "Error: " . $sql . "<br>" . $conn's error; 
     } 
 
 $sql = "INSERT INTO friends (player_id1, player_id2) VALUES ('" . $frienderID . "', '" . $friendeeID . "')";
 if ($conn->query($sql) === TRUE) { 
           echo "Friends entry created!"; 
     }else { 
-          echo "Error: " . $sql . "<br>" . $conn->error; 
+          echo "Error: " . $sql . "<br>" . $conn's error; 
     } 
 
 $conn->close();
